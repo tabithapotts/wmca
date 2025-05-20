e3f49dd494636ff7e35de2f111c4a1c7abd938d5
tabithapotts <tabitha.harman@gmail.com>
2025-05-20 20:50:09 +0100

    Added dark mode and light mode

---
+++ script.js
@@ -0,0 +1,9 @@
+function setDarkMode() {
+    let body = document.getElementById("body");
+    body.style = "background-color: black; color: white;"
+}
+
+function setLightMode() {
+    let body = document.getElementById("body");
+    body.style = "background-color: white; color: black;"
+}
